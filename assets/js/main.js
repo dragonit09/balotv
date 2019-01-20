4a7a85c61eceb25bb5bd686894983a9172243479
Draking153 <duynghia95@gmail.com>
2019-01-20 23:18:19 +0700

    Update width,  hide menu left on watch
    Update width,  hide menu left on watch

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,11 +14,26 @@ $( document ).ready(function() {
         e.preventDefault();
     });
     // #menu-toggle click
-    $( "body" ).on( "click", "#menu-toggle", function(e) {
-    	if($("body").hasClass("narrow"))
+    $( "body" ).on( "click", "#menu-toggle, #main-menu-overlay", function(e) {
+    	if($("body").hasClass("narrow")){ // Đang ẩn menu
     		$("body").removeClass("narrow");
-    	else 
+            if($("body").hasClass("menu-over")){
+                $("#main, #footer").append('<div id="main-menu-overlay" style="background: rgba(0, 0, 0, 0.5); position: absolute; width: 100%; height: 100%; top: 0; left: 0; z-index: 1;"></div>');
+                $("#header").addClass("un-boxshadow");
+                $("#footer").css({"z-index":0});
+                $("body").css({"overflow":"hidden"});
+            }
+        } else { // Menu đang show
     		$("body").addClass("narrow");
+            if($("body").hasClass("menu-over")){
+                $("div#main-menu-overlay").remove();
+                $("#header").removeClass("un-boxshadow");
+                $("#footer").css({"z-index":2});
+                $("body").css({"overflow":"auto"});
+            }
+        }
+
         e.preventDefault();
     });
+
 });
