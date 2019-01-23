9b8a1cd4c02a9c0b9ab939a03cdbbf3e03a41d33
Rent <minhtuancnttk39@gmail.com>
2019-01-24 00:07:52 +0700

    update last code for popup-response

--- assets/js/main.rent.js
+++ assets/js/main.rent.js
@@ -2,4 +2,19 @@ $(document).ready(function () {
     $("form.dropzone").dropzone({
         dictDefaultMessage: "Kéo và thả tệp tin",
     });
+
+    //----- OPEN POPUP
+    $( "body" ).on( "click", "[k-popup-open]", function(e) {
+        $('.popup').fadeOut(200);
+        var targeted_popup_class = $(this).attr('k-popup-open');
+        $('[k-popup="' + targeted_popup_class + '"]').fadeIn(100);
+        e.preventDefault();
+    });
+
+    //----- CLOSE POPUP
+    $( "body" ).on( "click", "[k-popup-close]", function(e) {
+        var targeted_popup_class = $(this).attr('k-popup-close');
+        $('[k-popup="' + targeted_popup_class + '"]').fadeOut(200);
+        e.preventDefault();
+    });
 });
