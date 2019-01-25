36ceb583da18d6851c1628654262d8eeb5838568
Rent <minhtuancnttk39@gmail.com>
2019-01-25 13:28:52 +0700

    Upload dropdown for categories, notifications and user info

--- assets/js/main.rent.js
+++ assets/js/main.rent.js
@@ -17,4 +17,43 @@ $(document).ready(function () {
         $('[k-popup="' + targeted_popup_class + '"]').fadeOut(200);
         e.preventDefault();
     });
+
+    var __isOpenNotify = false;
+    $("body").on('click', '#user-notifcation', function () {
+        __isOpenNotify = !__isOpenNotify;
+        var __this = $('.dialog-show__notifications');
+        if (__isOpenNotify) {
+            __this.fadeIn(300);
+            __this.removeClass('d-none').addClass('d-block');
+        } else {
+            __this.fadeOut(300);
+            __this.removeClass('d-block').addClass('d-none');
+        }
+    });
+
+    var __isOpenCategory = false;
+    $("body").on('click', '#menu-category', function () {
+        __isOpenCategory = !__isOpenCategory;
+        var __this = $('.dialog-show__categories');
+        if (__isOpenCategory) {
+            __this.fadeIn(300);
+            __this.removeClass('d-none').addClass('d-block');
+        } else {
+            __this.fadeOut(300);
+            __this.removeClass('d-block').addClass('d-none');
+        }
+    });
+
+    var __isOpenUserOption = false;
+    $("body").on('click', '#user-place', function () {
+        __isOpenUserOption = !__isOpenUserOption;
+        var __this = $('.dialog-show__user');
+        if (__isOpenUserOption) {
+            __this.fadeIn(300);
+            __this.removeClass('d-none').addClass('d-block');
+        } else {
+            __this.fadeOut(300);
+            __this.removeClass('d-block').addClass('d-none');
+        }
+    });
 });
